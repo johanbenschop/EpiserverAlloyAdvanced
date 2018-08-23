19155b6e6cdcb5996e29015f528cb0cd50adce0d
Johan Benschop <johan.benschop@indivirtual.com>
2018-08-23 10:12:48 +0200

    F4

---
+++ CMSTraining/Business/Initialization/PageFreshnessReportInitializationModule.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace AlloyAdvanced.Business.Initialization
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class PageFreshnessReportInitializationModule
+        : IInitializableModule
+    {
+        private bool initialized = false;
+
+        public void Initialize(InitializationEngine context)
+        {
+            if (!initialized)
+            {
+                RouteTable.Routes.MapRoute(
+                    name: "PageFreshnessReport",
+                    url: "pagefreshnessreport/{action}",
+                    defaults: new { controller = "PageFreshnessReport", action = "Index" });
+
+                initialized = true;
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context) { }
+    }
+}
